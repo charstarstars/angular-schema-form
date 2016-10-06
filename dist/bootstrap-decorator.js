093cc6e031d4f84d8d85037f6796cc0e8ce70313
Ariel Liu <aliu@lyft.com>
2016-10-06 11:19:02 -0700

    define removed

--- dist/bootstrap-decorator.js
+++ dist/bootstrap-decorator.js
@@ -1,7 +1,5 @@
 (function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['schemaForm'], factory);
-  } else if (typeof exports === 'object') {
+  if (typeof exports === 'object') {
     module.exports = factory(require('schemaForm'));
   } else {
     root.bootstrapDecorator = factory(root.schemaForm);
